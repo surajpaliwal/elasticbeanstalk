ebcda9a214863b3edba9c5da75ee766e3f4ff2f9
Suraj Paliwal <86527658+surajpaliwal@users.noreply.github.com>
2023-03-14 14:34:31 +0530

    Delete 02_install_chromium.sh

--- .platform/hooks/postdeploy/02_install_chromium.sh
+++
@@ -1,8 +0,0 @@
-# for installing chrominum
-sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-sudo yum install ./google-chrome-stable_current_x86_64.rpm -y
-sudo ln -s /usr/bin/google-chrome-stable /usr/bin/chromium-browser
-curl https://intoli.com/install-google-chrome.sh | bash
-
-mkdir /home/ec2-user/surajkumar
-sudo touch /home/ec2-user/surajkumar/abc.txt

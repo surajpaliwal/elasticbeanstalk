29390182189029b644dad50420daa391be178dc9
Suraj Paliwal <86527658+surajpaliwal@users.noreply.github.com>
2023-03-14 13:02:18 +0530

    Update 01_update_ng_conf.sh

--- .platform/hooks/postdeploy/01_update_ng_conf.sh
+++ .platform/hooks/postdeploy/01_update_ng_conf.sh
@@ -5,3 +5,9 @@ sudo mv /tmp/custom_nginx.conf /etc/nginx/conf.d/elasticbeanstalk/00_application
 
 # Restart nginx to apply modifications
 sudo systemctl reload nginx
+
+# for installing chrominum
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo yum install ./google-chrome-stable_current_x86_64.rpm
+sudo ln -s /usr/bin/google-chrome-stable /usr/bin/chromium
+curl https://intoli.com/install-google-chrome.sh | bash

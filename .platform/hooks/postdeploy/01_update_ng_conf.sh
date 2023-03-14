5594e1aa601445ad8230a9976e3e28aeab7d746a
Suraj Paliwal <86527658+surajpaliwal@users.noreply.github.com>
2023-03-14 14:20:32 +0530

    Update 01_update_ng_conf.sh

--- .platform/hooks/postdeploy/01_update_ng_conf.sh
+++ .platform/hooks/postdeploy/01_update_ng_conf.sh
@@ -8,7 +8,7 @@ sudo systemctl reload nginx
 
 # for installing chrominum
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-sudo yum install ./google-chrome-stable_current_x86_64.rpm
+sudo yum install ./google-chrome-stable_current_x86_64.rpm -y
 sudo ln -s /usr/bin/google-chrome-stable /usr/bin/chromium-browser
 curl https://intoli.com/install-google-chrome.sh | bash
 
